f1de16aa4e0a46df6b7619604da7f3364e489f6c
Catalin-Zaharia <catalinzaharia0112@gmail.com>
2021-11-05 15:07:02 +0200

    camera zoom

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
+    public Camera orthoCam;
     public Transform target;
     public Vector3 offset;
-    public float fixedY = 10;
+    public float zoomSpeed;
     void Start()
     {
+        orthoCam = Camera.main;
+        orthoCam.enabled = true;
+        orthoCam.orthographicSize = 9;
         target = GameObject.Find("Dummy").transform;
-        offset.x = 18.3f;
-        offset.z = -6.3f;
+        offset.x = 16.5f;
+        offset.z = -5.7f;
+        offset.y = 15;
+        zoomSpeed = 300f;
     }
 
+    void MoveCamera(){
+        transform.position = new Vector3 (target.position.x, 0, target.position.z)+ offset;
+        transform.LookAt(target);
+    }
+
+    void CheckZoom(){ //WIP
+        float elapsed = Time.deltaTime;
+        //float currentOrthoSize = orthoCam.orthographicSize;
+        orthoCam.orthographicSize = Mathf.Lerp(orthoCam.orthographicSize, orthoCam.orthographicSize-(1*Input.mouseScrollDelta.y), elapsed);
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (target.position.x, fixedY, target.position.z)+ offset;
+        MoveCamera();
+        CheckZoom();
     }
 }
