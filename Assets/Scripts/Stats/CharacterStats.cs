1d32130f8e25830c42bcfe7f39771923c4d70cec
costi-tica <costi13tica@gmail.com>
2022-01-03 21:23:52 +0200

    meh

--- Assets/Scripts/Stats/CharacterStats.cs
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 public abstract class CharacterStats : MonoBehaviour
 {
     public int MaxHealth = 100;
